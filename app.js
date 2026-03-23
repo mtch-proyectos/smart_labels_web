6b0fe4311efd736c7ee8c0585518e54c9ac48c53
Maria Teresa Chang <changmt@gmail.com>
2026-03-23 12:20:34 -0400

    Despliegue de pagina

--- app.js
+++ app.js
@@ -13,4 +13,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
+    module.exports = app;
 });
